5fc065b8f483a6209b1843dc31e34d35bfefc7af
FABRÍCIO FRANCA <08675636997@educacional.up>
2022-11-29 12:37:51 -0300

    adf

---
+++ Prova/API_Copa/Models/Jogo.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace API_Copa.Models
+{
+    public class Jogo
+    {
+        public Jogo()
+        {
+            SelecaoA = new Selecao();
+            SelecaoB = new Selecao();
+            CriadoEm = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public Selecao SelecaoA { get; set; }
+        public int SelecaoAId { get; set; }
+        public string SA { get; set; }
+        public int SelecaoBId { get; set; }
+        public Selecao SelecaoB { get; set; }
+        public string SB { get; set; }
+        public DateTime CriadoEm { get; set; }
+
+    }
+}
